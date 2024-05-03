ab67ae3456572e7733ca5daefc281c1dc1817b65
zuhair khan <zuhairkhan5134@gmail.com>
2024-05-03 12:28:00 +0530

    Merge remote changes

--- Frontend/frontend/src/pages/GoogleMap.jsx
+++ Frontend/frontend/src/pages/GoogleMap.jsx
@@ -1,30 +1,30 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+// import React, { Component } from 'react';
+// import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-class GoogleMap extends Component {
-  render() {
-    const mapStyles = {
-      width: '70%',
-      height: '65%'
-    };
+// class GoogleMap extends Component {
+//   render() {
+//     const mapStyles = {
+//       width: '70%',
+//       height: '65%'
+//     };
 
-    return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-          lat:18.967760, // Default latitude
-          lng:72.831120// Default longitude
-        }}
-      >
-        <Marker position={{ lat:18.967760, lng: 72.831120}} />
-      </Map>
-    );
-  }
-}
-export default GoogleApiWrapper({
-    apiKey: 'AIzaSyCN45VTnXCiZTVKDC7CG2ySoqPiefz9jLo'
-  })(GoogleMap);
+//     return (
+//       <Map
+//         google={this.props.google}
+//         zoom={14}
+//         style={mapStyles}
+//         initialCenter={{
+//           lat:18.967760, // Default latitude
+//           lng:72.831120// Default longitude
+//         }}
+//       >
+//         <Marker position={{ lat:18.967760, lng: 72.831120}} />
+//       </Map>
+//     );
+//   }
+// }
+// export default GoogleApiWrapper({
+//     apiKey: 'AIzaSyCN45VTnXCiZTVKDC7CG2ySoqPiefz9jLo'
+//   })(GoogleMap);
   
 
--- Frontend/frontend/src/pages/Location.jsx
+++ Frontend/frontend/src/pages/Location.jsx
@@ -1,14 +1,14 @@
 
-import GoogleMap from './GoogleMap';
-const Location = () => {
-  return (
-    <center>
-        <div className="ml-[250px] mt-[10px] mb-[100px] rounded-md" style={{ height: '400px', width: '100%' }}>
-      <GoogleMap />
-    </div>
-    </center>
+// import GoogleMap from './GoogleMap';
+// const Location = () => {
+//   return (
+//     <center>
+//         <div className="ml-[250px] mt-[10px] mb-[100px] rounded-md" style={{ height: '400px', width: '100%' }}>
+//       <GoogleMap />
+//     </div>
+//     </center>
 
-  )
-}
+//   )
+// }
 
-export default Location
+// export default Location
